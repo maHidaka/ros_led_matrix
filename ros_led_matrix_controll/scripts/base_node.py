c3764f462e1cb627714c3b2fb7d5798eb3d0d5e1
maHidaka <m.hdk816@gmail.com>
2022-11-12 13:42:52 +0900

    topic type をyamlからロードするように変更

--- ros_led_matrix_controll/scripts/base_node.py
+++ ros_led_matrix_controll/scripts/base_node.py
@@ -4,6 +4,7 @@
 from click import edit
 import rospy
 import yaml
+import sys
 import roslib.packages
 
 from std_msgs.msg import String
@@ -17,24 +18,34 @@ class BaseNode():
         self.pub = rospy.Publisher('/test2', String, queue_size=1)
 
     def import_yaml(self):
-        with open(roslib.packages.get_pkg_dir(self.pkg_name)+'/config.yaml') as yml:
-            config = yaml.load(yml)
-        rospy.loginfo('load config.yaml')
-        rospy.loginfo(config)
-        if(config['style']=='single'):
-            if(config['disp1']['type']=='topic'):
-                rospy.loginfo('trig1 attached topic: '+config['disp1']['trigger']['topic_name'])
-                self.sub = rospy.Subscriber(config['disp1']['trigger']['topic_name'], String, self.callback)
-            elif(config['disp1']['type']=='service'):
-                rospy.loginfo('trig1 is service')
-            else:
-                rospy.logerror('Unexpected value specified at "config.yaml disp1 type"')
+        
+        try:
+            with open(roslib.packages.get_pkg_dir(self.pkg_name)+'/config.yaml') as yml:
+                config = yaml.safe_load(yml)
+            rospy.loginfo('load config.yaml')
+            rospy.loginfo(config)
+            if(config['style']=='single'):
+                if(config['disp1']['type']=='topic'):
+                    type = globals()[config['disp1']['trigger']['topic_type']]
+                    rospy.loginfo('trig1 attached topic: '+config['disp1']['trigger']['topic_name'])
+                    self.sub = rospy.Subscriber(config['disp1']['trigger']['topic_name'], type, self.callback1)
+
+                elif(config['disp1']['type']=='service'):
+                    rospy.loginfo('trig1 is service')
+
+                else:
+                    rospy.logerr('Unexpected value specified at "config.yaml disp1 type"')
+
+        except Exception as e:
+            rospy.logerr('Exception occurred while loading YAML...')
+            rospy.signal_shutdown('Error')
+
             
 
         
     
 
-    def callback(self, data):
+    def callback1(self, data):
         self.publish(data)
 
     def publish(self, data):
